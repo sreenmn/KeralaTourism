e736e44808adbbd1eb54a086286ba68733bde727
sreenarayanan <sree_narayanan01@yahoo.com>
2020-04-01 16:41:45 +0530

    keralatourism front end added

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// var video = document.getElementById("myVideo");
+// var btn = document.getElementById("myBtn");
+
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "Pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "Play";
+//   }
+// }
+
+// $(document).ready(function(){
+//   $(".fancybox").fancybox({
+//         openEffect: "none",
+//         closeEffect: "none"
+//     });
+    
+//     $(".zoom").hover(function(){
+		
+// 		$(this).addClass('transition');
+// 	}, function(){
+        
+// 		$(this).removeClass('transition');
+// 	});
+// });
+    
